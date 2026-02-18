5263070e9036aae172e59d2bc9a4dec18f94e849
dws09plus <sudarwin3@gmail.com>
2026-02-17 22:10:23 -0800

    Added Login

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -1,7 +1,7 @@
 class SiteFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <!-- Footer: single source of truth -->
+            Footer: Darwin Su Set 1B A01075931
             <footer class="py-3 my-4 border-top text-center">
                 <p class="mb-0 text-muted">&copy; 2026 BCIT COMP1800</p>
             </footer>
